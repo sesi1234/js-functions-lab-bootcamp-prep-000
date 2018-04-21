0f1d82e256f1e62f6ba29daa3015c3a1ab468540
sesi_prosper@yahoo.co.uk <sesi_prosper@yahoo.co.uk>
2018-04-21 20:57:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ function happyHolidaysTo(name){
 function happyHolidayTo(holiday,name){
   return `Happy ${holiday}, ${name}!`
 }
-function happyHolidayTo(holiday,name){
+function holidayCountdown(holiday,days){
   return `Happy ${holiday}, ${name}!`
 }
